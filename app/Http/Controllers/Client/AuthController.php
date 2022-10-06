ce9fdeedb5e148fbdcf6806cb940090f9d5406c1
Jaba Khizanishvili <30653521+JabaKhizanishvili@users.noreply.github.com>
2022-10-06 17:02:05 +0400

    tst

--- app/Http/Controllers/Client/AuthController.php
+++ app/Http/Controllers/Client/AuthController.php
@@ -259,10 +259,11 @@ class AuthController extends Controller
     public function updateuser(Request $request)
     {
         if (isset($request->q)) {
-            $saveData = $request->except('q');
+            $saveData = $request->except(['q', '_token']);
+        } else {
+            $saveData = $request->except('_token');
         }
         dd($request->q, $saveData);
-        $saveData = $request->except('_token');
         $saveData['password'] = Hash::make($request->post('password'));
         $userid = User::where('id', Auth::user()->id)->update($saveData);
         return back()->with('success', 'success');
